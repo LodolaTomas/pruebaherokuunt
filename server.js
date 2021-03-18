0d91b4200140651799154376680970d7412b26a1
LodolaTomas <tomaslodola1@gmail.com>
2021-03-18 12:47:41 -0300

    Modificando jsons

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
+
 const app = express();
+
 const path = require('path');
 
-app.use(express.static(__dirname + '/angularapp'));
+app.use(express.static(__dirname + '/my-first-app'));
 
-app.listen(process.env.PORT||8080 );
-app.get('/*', function(req,res){
-    res.sendFile(path.join(__dirname + 'angularapp/index.html'));
+app.listen(process.env.PORT || 8080);
 
+app.get('/*', function(req, res) {
+
+    res.sendFile(path.join(__dirname + '/my-first-app/index.html'));
 });
 
-console.log('Build successful!!');
+console.log("Build");
