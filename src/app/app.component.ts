4102faf6597662697fe3b3b04cacdec86acd8a26
LodolaTomas <tomaslodola1@gmail.com>
2021-03-18 13:05:50 -0300

    Modificando jsons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,19 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
-cls(){
-  
-}
-
-calProm(val1:string,val2:string):number{
-  
-var nm1:number = parseInt(val1);
-var nm2:number = parseInt(val2);
-var sum:number=this.sumNum(nm1,nm2);
-return sum/2;
-}
-sumNum(val1:number,val2:number){
-  return val1+val2
-}
 
 }
