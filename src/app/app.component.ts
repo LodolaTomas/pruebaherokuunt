5ffb8c07ba1520bcbbe0efa7232d9b17c0d8c340
LodolaTomas <tomaslodola1@gmail.com>
2021-03-19 17:04:58 -0300

    cosas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title ="hello world"; 
+  title="hola mundo";
+  Edad1='';
+  Edad2='';
+  sum:number=0;
+  prom:number=0;
+
+  calcular(){
+    console.log(this.Edad1)
+    this.sum= parseInt(this.Edad1) + parseInt(this.Edad2)
+    this.prom =this.sum/2
+  }
+
+  limpiar(){
+    this.sum=0;
+    this.Edad1='';
+    this.Edad2='';
+    this.prom=0;
+  }
+
+
 }
